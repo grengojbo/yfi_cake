2ee574c2ddaf71b469244f6db5ac1abf3d86979e
dvdwalt <dvdwalt@4d75342d-bac5-42b9-a107-79493a2c96c2>
2011-11-28 02:59:45 +0000

    Heartbeat WIP
    git-svn-id: svn://svn.code.sf.net/p/hotcakes/code/trunk/yfi_cake@508 4d75342d-bac5-42b9-a107-79493a2c96c2

--- controllers/actions_controller.php
+++ controllers/actions_controller.php
@@ -14,7 +14,22 @@ class ActionsController extends AppController {
 
     var $scaffold;
 
-    function json_actions_for($nas_id=null){  //For the dojo Grid
+    function json_actions(){
+
+        $this->layout                     = 'ajax';
+
+        $json_return['items'] = array(
+                                        array('name'=>"Reload List","type"=>"reload","action"=>"reload"),
+                                       // array("name"=>"Edit Selected","type"=>"edit","action"=>"edit"),
+                                        array("name"=>"Add","type"=>"add","action"=>"add"),
+                                        array("name"=>"Delete Selected","type"=>"delete","action"=>"del")
+                                );
+
+        $json_return['json']['status']    = 'ok';
+        $this->set('json_return',$json_return);
+    }
+
+    function json_index($nas_id=null){  //For the dojo Grid
        
         $this->layout = 'ajax';
 
@@ -26,17 +41,43 @@ class ActionsController extends AppController {
         $json_return['items']             = array();
         //-----------------------------------------
 
-        $qr = $this->Action->find('all',array('conditions' => array('Action.na_id' => $nas_id)));
+        $qr = $this->Action->find('all',array('conditions' => array('Action.na_id' => $nas_id),'order' => array('Action.modified DESC')));
         foreach($qr as $item){
            // print_r($item);
             array_push($json_return['items'],$item['Action']);
         }
 
-
-
         //--Return the JSON --------------------
         $this->set('json_return',$json_return);
         //--------------------------------------
+    }
+
+     function json_add($id){
+
+        $this->layout           = 'ajax';
+        $command                = $this->params['form']['command']; 
+        $d                      = array();
+        $d['Action']['na_id']   = $id;
+        $d['Action']['command'] = $command;
+        $this->Action->save($d);
+        $json_return['json']['status']      = 'ok';
+        $this->set('json_return',$json_return);
+    }
+
+    function json_del(){
+        $this->layout = 'ajax';
+
+        foreach(array_keys($this->params['url']) as $key){
+            if(preg_match('/^\d+/',$key)){
+                //----------------
+                $id = $this->params['url'][$key];
+                $this->Action->delete($id);
+            }
+        }
+    
+        $json_return= array();
+        $json_return['json']['status'] = "ok";
+        $this->set('json_return',$json_return);
     }
 
 }
