0ee8d91d1b64d6074928ae4edbc832a536ba1e1f
dvdwalt <dvdwalt@4d75342d-bac5-42b9-a107-79493a2c96c2>
2012-09-10 20:33:39 +0000

    Credit Card transaction detail view
    git-svn-id: svn://svn.code.sf.net/p/hotcakes/code/trunk/yfi_cake@624 4d75342d-bac5-42b9-a107-79493a2c96c2

--- setup/db/cc_transactions.sql
+++ setup/db/cc_transactions.sql
@@ -24,3 +24,21 @@ CREATE TABLE `transaction_details` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+
+DROP TABLE IF EXISTS `expiry_changes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `expiry_changes` (
+  `id` char(36) NOT NULL,
+  `cc_transaction_id` char(36) NOT NULL DEFAULT '',
+  `initiator_id` char(36) NOT NULL DEFAULT '',
+  `owner_id` char(36) DEFAULT NULL,
+  `realm_id` char(36) NOT NULL DEFAULT '',
+  `old_value` bigint(20) DEFAULT NULL,
+  `new_value` bigint(20) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
