551d5844e6b546b7a7b1ca69bad701bc369cb39a
dvdwalt <dvdwalt@4d75342d-bac5-42b9-a107-79493a2c96c2>
2012-10-18 08:56:08 +0000

    add a check for heartbeats
    git-svn-id: svn://svn.code.sf.net/p/hotcakes/code/trunk/yfi_cake@640 4d75342d-bac5-42b9-a107-79493a2c96c2

--- controllers/heartbeats_controller.php
+++ controllers/heartbeats_controller.php
@@ -8,7 +8,33 @@ class HeartbeatsController extends AppController {
     var $uses       = array('Heartbeat','Na','Action');
 
      function beforeFilter() {
-       $this->Auth->allow('json_hello');
+       $this->Auth->allow('json_hello','active_check');
+    }
+
+    function active_check($mac){
+        $this->layout = 'text';
+        //Check if the MAC is in the correct format
+        $pattern = '/^([0-9a-fA-F]{2}[-]){5}[0-9a-fA-F]{2}$/i';
+        if(preg_match($pattern, $mac)< 1){
+            $error = "ERROR: MAC missing or wrong";
+            $this->set('error',$error);
+            return;
+        }
+
+        //We use a convention of 08-00-27-56-22-0B_ce5b65b780ae42d937294b2aa166d608 in the community field
+        //in the nas table. We specify a MAC with an underscore initially and when the device register itself
+        //We add the device generated password
+        $r = $this->Na->find('first',array('conditions' =>array('Na.community' => $mac)));
+
+        if($r == ''){
+            $return_string = "HEARTBEAT=NO";
+        }else{
+            $return_string = "HEARTBEAT=".$r['Na']['nasname'];
+        }
+
+        //--Return the actions (if required) ---
+        $this->set('return_string',$return_string);
+        //--------------------------------------
     }
 
     function json_hello($mac){
